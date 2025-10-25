1c40527ba68915628b64573dbe9b8cc050a75466
Carlos Roberto Ferreira Alves <carloscorretor15@gmail.com>
2025-10-25 18:32:36 -0300

    Verificando a saúde

--- crc/obtencao_dados.py
+++ crc/obtencao_dados.py
@@ -1,27 +1,37 @@
 import requests
 
 def obter_token_jwt():
-    # URL base da API
     BASE_URL = "http://ec2-52-67-119-247.sa-east-1.compute.amazonaws.com:8000"
     LOGIN_URL = f"{BASE_URL}/login"
 
-    # Credenciais
     credentials = {
         "username": "kaizen-poke",
         "password": "4w9f@D39fkkO"
     }
 
-    # Fazendo login (enviando JSON)
     response = requests.post(LOGIN_URL, json=credentials)
     return response
 
-
 response = obter_token_jwt()
+token = response.json().get("access_token")
 
-if response.status_code == 200:
-    token = response.json().get("access_token")
-    print("✅ Login realizado com sucesso!")
-    print("Token JWT:", token)
+if token:
+    
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get("http://ec2-52-67-119-247.sa-east-1.compute.amazonaws.com:8000/health", headers=headers)
+
+    if response.status_code == 200:
+        print("Acesso autorizado!")
+        print(response.json())
+    else:
+        print("Erro ao acessar recurso:", response.status_code)
+        print(response.text)
+
 else:
-    print("❌ Erro ao autenticar:", response.status_code)
-    print(response.text)
+    print("Token não obtido, verifique as credenciais.")
+
+    
+    
